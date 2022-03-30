4ef8952ad59205fb30cb5259fe013b34031a9eab
siddharth <jsidd06@gmail.com>
2022-03-30 18:57:19 +0530

    add root color on full screen

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet,View } from 'react-native';
 import StartGame from './components/screens/StartGame';
 
 export default function App() {
   return (
-    <StartGame />
+    <View style={styles.rootScreen}>
+      <StartGame />
+    </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+    backgroundColor: '#ddb52f',
+  }
+});
