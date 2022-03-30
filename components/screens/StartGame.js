afccc8c531a0322ccd1f111e80ac07609203b828
siddharth <jsidd06@gmail.com>
2022-03-30 18:52:01 +0530

    button style change

--- components/screens/StartGame.js
+++ components/screens/StartGame.js
@@ -3,18 +3,32 @@ import PrimaryButton from "../PrimaryButton"
 
 function StartGame () {
     return (
-        <View style={styles.inputContainer}>
-            <TextInput style={styles.numberInput} keyboardType="number-pad" autoCorrect={false}  autoCapitalize="none" maxLength={2} />
-            <PrimaryButton >Reset</PrimaryButton>
-            <PrimaryButton >Confirm</PrimaryButton>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.numberInput}
+          keyboardType="number-pad"
+          autoCorrect={false}
+          autoCapitalize="none"
+          maxLength={2}
+        />
+        <View style={styles.buttonsContainer}>
+          <View style={styles.buttonContainer}>
+            <PrimaryButton>Reset</PrimaryButton>
+          </View>
+          <View style={styles.buttonContainer}>
+            <PrimaryButton>Confirm</PrimaryButton>
+          </View>
         </View>
-    )
+      </View>
+    );
 }
 
 export default StartGame
 
 const styles = StyleSheet.create({
   inputContainer: {
+      justifyContent: "center",
+        alignItems: "center",
     marginTop: 100,
     marginHorizontal: 24,
     padding: 16,
@@ -36,5 +50,11 @@ const styles = StyleSheet.create({
         color: "#ddb52f",
         fontWeight: "bold",
         marginVertical: 8,
+  },
+  buttonsContainer:{
+        flexDirection: "row",
+  },
+  buttonContainer:{
+        flex: 1,
   }
 });
