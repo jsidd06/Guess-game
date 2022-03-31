54c29f82be6fd98720727f2b91ca08740118c7bf
siddharth <jsidd06@gmail.com>
2022-03-31 09:24:55 +0530

    add gamescreen as a testing

--- components/screens/GameScreen.js
+++ components/screens/GameScreen.js
@@ -1,9 +1,24 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet ,Text} from "react-native";
 
 function GameScreen() {
-  return <View style={styles}></View>;
+  return (
+    <View style={styles.screenContainer}>
+      <Text>Opponent's Guess</Text>
+      {/* GUESS */}
+      <View>
+        <Text>Higher or Lower?</Text>
+        {/* + - */}
+      </View>
+      {/* <View>LOG ROUNDS</View> */}
+    </View>
+  );
 }
 
 export default GameScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screenContainer: {
+    flex: 1,
+    padding: 10,
+  }
+});
