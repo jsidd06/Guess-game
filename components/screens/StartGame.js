09af4630e2bf9b1a83f2a88daa566b721303a128
siddharth <jsidd06@gmail.com>
2022-03-31 08:01:02 +0530

    lineargrdient package install and use it and styles change

--- components/screens/StartGame.js
+++ components/screens/StartGame.js
@@ -1,60 +1,60 @@
-import {View,StyleSheet,TextInput} from "react-native"
-import PrimaryButton from "../PrimaryButton"
+import { View, StyleSheet, TextInput } from "react-native";
+import PrimaryButton from "../PrimaryButton";
 
-function StartGame () {
-    return (
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.numberInput}
-          keyboardType="number-pad"
-          autoCorrect={false}
-          autoCapitalize="none"
-          maxLength={2}
-        />
-        <View style={styles.buttonsContainer}>
-          <View style={styles.buttonContainer}>
-            <PrimaryButton>Reset</PrimaryButton>
-          </View>
-          <View style={styles.buttonContainer}>
-            <PrimaryButton>Confirm</PrimaryButton>
-          </View>
+function StartGame() {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        keyboardType="number-pad"
+        autoCorrect={false}
+        autoCapitalize="none"
+        maxLength={2}
+      />
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Confirm</PrimaryButton>
         </View>
       </View>
-    );
+    </View>
+  );
 }
 
-export default StartGame
+export default StartGame;
 
 const styles = StyleSheet.create({
   inputContainer: {
-      justifyContent: "center",
-        alignItems: "center",
+    justifyContent: "center",
+    alignItems: "center",
     marginTop: 100,
     marginHorizontal: 24,
     padding: 16,
     borderRadius: 8,
-    backgroundColor: "#4e0329",
+    backgroundColor: "#3b021f",
     elevation: 4,
     shadowColor: "black",
     shadowOpacity: 0.25,
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
   },
-  numberInput:{
-      height: 50,
-        width: 50,
-        fontSize:32,
-        borderBottomColor: "#ddb52f",
-        borderBottomWidth: 2,
-        textAlign: "center",
-        color: "#ddb52f",
-        fontWeight: "bold",
-        marginVertical: 8,
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    textAlign: "center",
+    color: "#ddb52f",
+    fontWeight: "bold",
+    marginVertical: 8,
   },
-  buttonsContainer:{
-        flexDirection: "row",
+  buttonsContainer: {
+    flexDirection: "row",
   },
-  buttonContainer:{
-        flex: 1,
-  }
+  buttonContainer: {
+    flex: 1,
+  },
 });
