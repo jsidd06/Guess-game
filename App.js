f9cc995634b5e044b4a5a48faa7f406990b3d0e3
siddharth <jsidd06@gmail.com>
2022-03-31 08:29:40 +0530

    add background image with change the style

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet,ImageBackground } from 'react-native';
 import {LinearGradient} from "expo-linear-gradient"
 import StartGame from './components/screens/StartGame';
 
 export default function App() {
   return (
-    <LinearGradient colors={['#4e0329','#ddb52f']} style={styles.rootScreen}>
-      <StartGame />
+    <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.rootScreen}>
+      <ImageBackground
+        source={require("./assets/Images/background.png")}
+        resizeMode="cover"
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImg}
+      >
+        <StartGame />
+      </ImageBackground>
     </LinearGradient>
   );
 }
@@ -13,5 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
+  },
+  backgroundImg:{
+    opacity:0.15,
   }
 });
