dfc2d32cd8f2c67325820c1f51fda58ef24f6957
siddharth <jsidd06@gmail.com>
2022-03-31 10:35:11 +0530

    add a math logic user stype number and submit output will be changes on next screen

---
+++ components/Game/NumberContainer.js
@@ -0,0 +1,29 @@
+import { View, StyleSheet, Text } from "react-native";
+import Colors from "../constants/Colors";
+
+function NumberContainer({ children }) {
+  return (
+      <View style={styles.container}>
+    <Text style={styles.numText}>{children}</Text>
+  </View>
+  )
+}
+
+export default NumberContainer;
+
+const styles = StyleSheet.create({
+  container: {
+    borderWidth: 4,
+    borderColor: Colors.accent500,
+    margin: 24,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 24,
+    borderRadius: 8,
+  },
+  numText: {
+    color: Colors.accent500,
+    fontSize: 36,
+    fontWeight: "bold",
+  },
+});
--- components/screens/GameScreen.js
+++ components/screens/GameScreen.js
@@ -1,12 +1,29 @@
 import { View, StyleSheet ,Text} from "react-native";
+import NumberContainer from "../Game/NumberContainer";
+import {useState} from "react";
 import Title from "../Title";
 
-function GameScreen() {
+function generateRandomBetween(min, max, exclude) {
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+}
+
+
+function GameScreen({userChoice}) {
+  const initialGuess = generateRandomBetween(1, 100, userChoice);
+const [userGuess,setUserGuess] = useState(initialGuess)
+
+
   return (
     <View style={styles.screenContainer}>
       
         <Title>Opponent's Guess</Title>
-      
+      <NumberContainer>{userGuess}</NumberContainer>
       {/* GUESS */}
       <View>
         <Text>Higher or Lower?</Text>
